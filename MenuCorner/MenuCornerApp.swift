538663cfd189b4c6176b46e7c0ff4de725cb9d0a
Andrej Kling <boxingprofi@web.de>
2021-01-16 17:08:01 +0100

    Initial Commit

---
+++ MenuCorner/MenuCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuCornerApp.swift
+//  MenuCorner
+//
+//  Created by Andrej Kling on 16.01.21.
+//
+
+import SwiftUI
+
+@main
+struct MenuCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
